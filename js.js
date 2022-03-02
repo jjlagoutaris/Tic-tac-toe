629af815d382e3d9d0173d5f1d447efc01cd7834
John Lagoutaris <jjlagoutaris@gmail.com>
2022-03-02 04:38:44 -0500

    I added the user ability to play the game as well as checks to see if the game has been won or tied

--- js.js
+++ js.js
@@ -1,29 +1,120 @@
+const Player = (symbol) => {
+    let score = 0;
+    const getScore = () => score;
+    return {getScore, symbol};
+};
+
+const setup = () => {
+    const playerOne = Player('X');
+    const playerTwo = Player('O');
+    let turn = 1;
+    return {playerOne, playerTwo, turn};
+};
+
 const gameBoard = (() => {
-    let boardArr = [['X', '', ''],['X', 'O', ''],['', '', '']];
+    let boardArr = ['', '', '', '', '', '', '', '', ''];
     return{boardArr};    
 })();
 
 const displayController = ((board) => {
     const cells = document.querySelectorAll('.grid-cell');
-    let cellCounter = 0;
-    const updateCells = () => {
-        for(let i = 0; i < 3; i++){
-            for(let j = 0; j < 3; j++){
-                cells[cellCounter].textContent = board[i][j];
-                cellCounter++;
-            }
+
+
+    const setCells = () => {
+        for(let i = 0; i < board.length; i++){
+            cells[i].textContent = board[i];
+            cells[i].setAttribute('data-index', `${i}`);
+            // console.log(cells[i].getAttribute);
         }
-        cellCounter = 0;
     };
-    return{updateCells};
+    return{setCells};
 })(gameBoard.boardArr);
 
-const Player = (symbol) => {
-    let score = 0;
-    const getScore = () => score;
-    return {getScore};
-}
+const gameLoop = ((board) => {
+    const cells = document.querySelectorAll('.grid-cell');
+    let details = setup();
 
-const gameLoop = (() => {
-    displayController.updateCells();
-})();
+    const clickCells = () => {
+        for(const cell of cells){
+            cell.addEventListener('click', (e) =>{
+                if(cell.textContent === ''){
+                    if (details.turn === 1){
+                        board[e.target.getAttribute('data-index')] = details.playerOne.symbol;
+                        cell.textContent = details.playerOne.symbol;
+                        logVictory();
+                        details.turn = 2;
+                    }
+                    else{
+                        board[e.target.getAttribute('data-index')] = details.playerTwo.symbol;
+                        cell.textContent = details.playerTwo.symbol;
+                        logVictory();
+                        details.turn = 1;
+                    }
+                }
+            });
+        }
+    }
+
+    const checkGameState = () => {
+        let outcome = 'unresolved';
+        if(((board[0] === details.playerOne.symbol) && (board[1] === details.playerOne.symbol) && 
+        (board[2] === details.playerOne.symbol)) || ((board[0] === details.playerTwo.symbol) && (board[1] === details.playerTwo.symbol) && 
+        (board[2] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[3] === details.playerOne.symbol) && (board[4] === details.playerOne.symbol) && 
+        (board[5] === details.playerOne.symbol)) || ((board[3] === details.playerTwo.symbol) && (board[4] === details.playerTwo.symbol) && 
+        (board[5] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[6] === details.playerOne.symbol) && (board[7] === details.playerOne.symbol) && 
+        (board[8] === details.playerOne.symbol)) || ((board[6] === details.playerTwo.symbol) && (board[7] === details.playerTwo.symbol) && 
+        (board[8] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[0] === details.playerOne.symbol) && (board[4] === details.playerOne.symbol) && 
+        (board[8] === details.playerOne.symbol)) || ((board[0] === details.playerTwo.symbol) && (board[4] === details.playerTwo.symbol) && 
+        (board[8] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[2] === details.playerOne.symbol) && (board[4] === details.playerOne.symbol) && 
+        (board[6] === details.playerOne.symbol)) || ((board[2] === details.playerTwo.symbol) && (board[4] === details.playerTwo.symbol) && 
+        (board[6] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[0] === details.playerOne.symbol) && (board[3] === details.playerOne.symbol) && 
+        (board[6] === details.playerOne.symbol)) || ((board[0] === details.playerTwo.symbol) && (board[3] === details.playerTwo.symbol) && 
+        (board[6] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[1] === details.playerOne.symbol) && (board[4] === details.playerOne.symbol) && 
+        (board[7] === details.playerOne.symbol)) || ((board[1] === details.playerTwo.symbol) && (board[4] === details.playerTwo.symbol) && 
+        (board[7] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if(((board[2] === details.playerOne.symbol) && (board[5] === details.playerOne.symbol) && 
+        (board[8] === details.playerOne.symbol)) || ((board[2] === details.playerTwo.symbol) && (board[5] === details.playerTwo.symbol) && 
+        (board[8] === details.playerTwo.symbol))){
+            outcome = 'victory';
+        }
+        else if((board[0] !== '') && (board[1] !== '') && (board[2] !== '') && (board[3] !== '') && (board[4] !== '') && (board[5] !== '') && (board[6] !== '')
+        && (board[7] !== '')  && (board[8] !== '')){
+            outcome = 'tie';
+        }
+        return outcome;
+    }
+
+    const logVictory = () => {
+        let outcome = checkGameState();
+        if (outcome === 'victory'){
+            console.log(`Player ${details.turn} wins!`);
+        }
+        else if (outcome === 'tie'){
+            console.log(`It's a draw`);
+        }
+    }
+
+    displayController.setCells();
+    clickCells();
+
+})(gameBoard.boardArr);
