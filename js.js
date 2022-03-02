825b70b2fe92ea7db32ac875b92a166bc17e7ea0
John Lagoutaris <jjlagoutaris@gmail.com>
2022-03-02 01:47:47 -0500

    I have hooked the displayController up to the gameLoop & hooked up the displayController to the gameBoard so that the boardArr's values are plugged into the displayController and updated.

--- js.js
+++ js.js
@@ -1,18 +1,29 @@
-const gameLoop = (function(){
-    
-})();
-
-const gameBoard = (function(){
-    let boardArr = [['X', 'O', 'X'],['O', 'O', 'X'],['X', 'O', 'X']];
+const gameBoard = (() => {
+    let boardArr = [['X', '', ''],['X', 'O', ''],['', '', '']];
     return{boardArr};    
 })();
 
-const displayController = (function(board){
-    return{};
-})();
+const displayController = ((board) => {
+    const cells = document.querySelectorAll('.grid-cell');
+    let cellCounter = 0;
+    const updateCells = () => {
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                cells[cellCounter].textContent = board[i][j];
+                cellCounter++;
+            }
+        }
+        cellCounter = 0;
+    };
+    return{updateCells};
+})(gameBoard.boardArr);
 
 const Player = (symbol) => {
     let score = 0;
     const getScore = () => score;
     return {getScore};
 }
+
+const gameLoop = (() => {
+    displayController.updateCells();
+})();
